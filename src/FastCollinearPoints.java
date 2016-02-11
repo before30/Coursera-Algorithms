99fd728a9bd74cbb6343a57535746c73f0033367
before30 <before30@gmail.com>
2016-02-11 23:42:33 +0900

    start of 3rd homework

---
+++ src/FastCollinearPoints.java
@@ -0,0 +1,19 @@
+/**
+ * Created by before30 on 2016. 2. 11..
+ */
+public class FastCollinearPoints {
+    // finds all line segments containing 4 or more points
+    public FastCollinearPoints(Point[] points) {
+
+    }
+
+    // the number of line segments
+    public int numberOfSegments() {
+        return -1;
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
+        return null;
+    }
+}
